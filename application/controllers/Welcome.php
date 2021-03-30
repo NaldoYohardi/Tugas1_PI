e657ac9c322076bf3926009eade16a114c69667e
aldrend <liongod452.aw@gmail.com>
2021-03-30 23:22:47 +0700

    Update to include PHP

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -20,6 +20,6 @@ class Welcome extends CI_Controller {
 	 */
 	public function index()
 	{
-		$this->load->view('auth/login.html');
+		$this->load->view('auth/login.php');
 	}
 }
--- application/views/auth/regist.php
+++ application/views/auth/regist.php
@@ -5,14 +5,12 @@
     <title> Registrasi </title>
   </head>
   <body>
-    <form action="regist.php" method="POST">
+    <?php if($this->session->flashdata('errors')): ?>
+     <div class="alert alert-danger"><?php echo $this->session->flashdata('errors') ?>
+  <?php endif; ?>
+    <form action="<?php echo base_url('userdata/regist')?>" method="POST">
       <fieldset>
         <legend> Registrasi </legend>
-        <p>
-          <label> Nama: </label>
-          <input type="text" name="name" placeholder="Full Name..." />
-        </p>
-
         <p>
           <label> Username: </label>
           <input type="text" name="username" placeholder="Username..." />
@@ -30,8 +28,12 @@
 
         <p>
           <label> Jenis Kelamin: </label>
-          <label><input type="radio" name="jenis_kelamin" value="laki-laki" /> Laki-laki </label>
-          <label><input type="radio" name="jenis_kelamin" value="perempuan" /> Perempuan </label>
+          <label><input type="radio" name="jenkel" value="0" /> Laki-laki </label>
+          <label><input type="radio" name="jenkel" value="1" /> Perempuan </label>
+        </p>
+
+        <p>
+          <input type="submit" name="submit" value="regist">
         </p>
       </fieldset>
     </form>
