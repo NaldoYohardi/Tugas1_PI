b1ecfa2b4bb57be075bdeb29f2cb0a367421b583
aldrend <liongod452.aw@gmail.com>
2021-03-31 10:56:37 +0700

    delete additional lines of codes

--- application/views/auth/login.php
+++ application/views/auth/login.php
@@ -1,53 +1,4 @@
 <!DOCTYPE html>
-<<<<<<< HEAD
-<html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title>Form Login</title>
-  </head>
-
-  <body>
-    <?php if($this->session->flashdata('errors')): ?>
-     <div class="alert alert-danger"><?php echo $this->session->flashdata('errors') ?>
-  <?php endif; ?>
-    <form action="<?php echo base_url('userdata/do_login') ?>" method="POST">
-      <fieldset>
-      <legend> Login </legend>
-
-      <p>
-        <label> Username: </label>
-        <input type="text" name="username" placeholder="username..."/>
-      </p>
-
-      <p>
-        <label> Password: </label>
-        <input type="password" name="password" placeholder="password..."/>
-      </p>
-
-      <p>
-        <input type="submit" name="submit" value="Login"/>
-      </p>
-
-      <p>
-        Remember me  <input type="checkbox" name="remember" value="0" id="remember" onclick="set()">
-
-      </p>
-      </fieldset>
-</form>
-<a href="<?php echo base_url('userdata/register') ?>"><input type="submit" name="submit" value="Register"/></a>
-  </body>
-  <script type="text/javascript">
-
-      function set(){
-        if(document.getElementById("remember").value == 0) {
-          document.getElementById("remember").value = 1;
-        } else {
-          document.getElementById("remember").value = 0;
-
-        }
-      }
-  </script>
-=======
 <html lang="en">
 <head>
     <meta charset="utf-8">
@@ -92,9 +43,9 @@
                           <label><input type="checkbox" name="remember" value="remember"/> Remember me </label>
                         </li>
                         <li>
-                            <center>
-                            <input type="submit" value="Login" class="button" />
-                            </center>
+                          <center>
+                          <input type="submit" value="Login" class="button" />
+                          </center>
                         </li>
                     </ul>
                 </form>
@@ -147,5 +98,4 @@
       }
     }
 </script>
->>>>>>> UI/UX
 </html>
